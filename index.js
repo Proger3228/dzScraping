bc61e49cf191d406e630388792f314a68edafa77
Proger3228 <damir221003@gmail.com>
2020-05-12 18:19:57 +0400

    Add server response to pinging

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { Telegraf } = require( "telegraf" );
 const CheckUpdates = require( "./utils/notifying.js" );
 const { hasUser, addUser } = require( "./utils/users.js" );
+const app = require( "express" )()
+const fs = require( "fs" );
+const path = require( "path" );
 
 const bot = new Telegraf( process.env.TOKEN );
 
@@ -12,8 +15,11 @@ bot.command( "start", ( ctx ) => {
     }
 } )
 
-setInterval( CheckUpdates, 1000 * 60 );
-
 bot.startPolling( 3000 );
 
+setInterval( CheckUpdates, 1000 * 60 );
+
+app.get( "/", ( req, res ) => res.send( JSON.parse( fs.readFileSync( path.resolve( "", "./data.json" ) ).toString() ) ) );
+
+app.listen( process.env.PORT || 3000 );
 
