3b18cba3ed80b896c0f22d545c9f9f44f2f20072
Proger3228 <damir221003@gmail.com>
2020-05-12 18:06:16 +0400

    --amend

--- utils/users.js
+++ utils/users.js
@@ -3,35 +3,42 @@ const path = require( 'path' );
 
 const dataPath = "./data.json"
 
+const addUser = ( user ) => {
+    if ( !validateUser( user ) ) throw new TypeError( "user must be a valid user" )
+
+    const file = JSON.parse( fs.readFileSync( path.resolve( "", dataPath ) ).toString() );
+
+    const users = file.users;
+    users.push( user );
+
+    fs.writeFileSync( path.resolve( "", dataPath ), JSON.stringify( { ...file, users } ) );
+}
+
+const hasUser = ( id ) => {
+    const users = getUsers();
+
+    return users.some( user => user.id === id );
+}
+
+const getUsers = () => {
+    const file = fs.readFileSync( path.resolve( "", dataPath ) ).toString();
+    const object = JSON.parse( file );
+
+    return object.users;
+}
+const validateUser = ( user ) => {
+    if ( user.toString() !== "[object Object]" ) return false;
+
+    return (
+        "id" in user && typeof user.id === "number" &&
+        "first_name" in user && typeof user.first_name === "string" &&
+        "last_name" in user && typeof user.last_name === "string"
+    )
+}
+
 module.exports = {
-    addUser: ( user ) => {
-        if ( !this.validateUser( user ) ) throw new TypeError( "user must be a valid user" )
-
-        const file = JSON.parse( fs.readFileSync( path.resolve( "", dataPath ) ).toString() );
-
-        const users = file.users;
-        users.push( user );
-
-        fs.writeFileSync( path.resolve( "", dataPath ), JSON.stringify( { ...file, users } ) );
-    },
-    hasUser: ( id ) => {
-        const users = this.getUsers();
-
-        return users.some( user => user.id === id );
-    },
-    getUsers: () => {
-        const file = fs.readFileSync( path.resolve( "", dataPath ) ).toString();
-        const object = JSON.parse( file );
-
-        return object.users;
-    },
-    validateUser: ( user ) => {
-        if ( user.toString() !== "[object Object]" ) return false;
-
-        return (
-            "id" in user && typeof user.id === "number" &&
-            "first_name" in user && typeof user.first_name === "string" &&
-            "last_name" in user && typeof user.last_name === "string"
-        )
-    },
+    addUser,
+    hasUser,
+    getUsers,
+    validateUser
 }
